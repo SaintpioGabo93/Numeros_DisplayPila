1e9e937df32b56619e55ccb8ed81434d4eba9374
Saintpiogabo93 <tocata_fuga_dorian@hotmail.com>
2024-12-26 20:54:29 -0600

    Se crea el proyecto en para contador en ensambador

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Numeros_DisplayPila.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Numeros_DisplayPila.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=numerosdisplaypila.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/numerosdisplaypila.x.tar
